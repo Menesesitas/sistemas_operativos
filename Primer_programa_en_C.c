32b9dd8814a59ba95391ce1dd2f888a84b8856a4
Fernando Uriel Perez Meneses <122332137+Menesesitas@users.noreply.github.com>
2023-01-13 17:30:43 -0600

    Add files via upload

---
+++ Primer_programa_en_C.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+// este código te dará el resultado del área de un rectángulo
+int main() 
+{
+	float base_b;
+	float altura_h;
+	float resultado_r;
+	
+	printf("Ingresa una medida para las base:\n");
+	scanf("%f", &base_b);
+	printf("Ingresa una medida para las altura:\n");
+	scanf("%f", &altura_h);
+	
+	resultado_r = base_b * altura_h;
+	printf("El área del rectángulo es igual a= %f", resultado_r);
+	
+	return 0; 
+}
+
