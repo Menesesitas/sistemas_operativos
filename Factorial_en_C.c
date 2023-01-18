6a9cb4bf8c1c5a47085a80982b5c2e7bfe78f906
Fernando Uriel Perez Meneses <122332137+Menesesitas@users.noreply.github.com>
2023-01-17 23:27:09 -0600

    Add files via upload

---
+++ Factorial_en_C.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+//Este código dará el factorial de cualquier número
+int f(int n);
+int main() 
+{
+	int n, r;
+	printf("Ingrese el número del que quiere el factorial\n");
+	scanf("%i", &n);
+	r = f(n);
+	
+	printf("f = %d", r);
+	
+	return 0;
+}
+	
+int f(int n)
+{
+	if (n != 0)
+		return n * f(n-1);
+	else 
+	return 1;
+}
+
