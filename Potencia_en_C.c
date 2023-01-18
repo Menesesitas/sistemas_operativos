6f2be7f6763cbe764670f3f98be744b02b31565d
Fernando Uriel Perez Meneses <122332137+Menesesitas@users.noreply.github.com>
2023-01-17 23:23:44 -0600

    Add files via upload

---
+++ Potencia_en_C.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+	int num_n = 1;
+	int potencia_p = 0;
+	while(num_n < 30) 
+	{
+		num_n *= 2;
+		potencia_p++;
+	}
+	printf("La potencia de 2 mayor que 30 es: 2^%d = %d", potencia_p, num_n);
+	return 0;
+}
+
