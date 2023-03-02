4fd0f211040f6cfd7eebaea01234bed5ad915fd4
Fernando Uriel Perez Meneses <122332137+Menesesitas@users.noreply.github.com>
2023-03-02 12:58:09 -0600

    Add files via upload

---
+++ Ejemplo_de_execl/Ejemplo_execl.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+int main()
+{
+	pid_t pid_A;
+	int status;
+	
+	pid_A = fork();
+	
+	// Child process
+	if( pid_A == 0 )
+	{
+		execl("/home/menesesitas/Escritorio/hello_world", "", NULL);
+		perror("excecl ha fallado.");
+		return 0;
+	}
+		else
+	{
+		wait(&status);
+		if( WEXITSTATUS(status) == 0 )
+		{
+			printf("Mi hijo se ha ejecutado exitosamente.\n");
+		}
+	}
+		return 0;
+}
---
+++ Ejemplo_de_execl/hello_world.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("¡Hola mundo de C!\n");
+	return 0;
+}
+
